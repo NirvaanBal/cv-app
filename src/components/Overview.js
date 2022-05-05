84a541708fba632355ada2a756395b893425ebe3
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-05-05 10:02:18 +0530

    Add styles to Overview component

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Overview.css';
 
 class Overview extends Component {
   constructor(props) {
@@ -11,27 +12,33 @@ class Overview extends Component {
     return (
       <div className="overview">
         <h2>{name}</h2>
-        <p>{email}</p>
-        <p>{phone}</p>
+        <div className="e-p">
+          <p>{email}</p>
+          <p>{phone}</p>
+        </div>
+        <h3>Education</h3>
         <div className="edu">
           {this.props.education.map((course, index) => {
             return (
               <div className="course" key={index}>
-                <p>{course.course}</p>
+                <h4>{course.course}</h4>
                 <p>{course.school}</p>
                 <p>{course.year}</p>
               </div>
             );
           })}
         </div>
+        <h3>Experience</h3>
         <div className="exp">
           {this.props.experience.map((job, index) => {
             return (
               <div className="job" key={index}>
-                <p>{job.job}</p>
+                <h4>{job.job}</h4>
                 <p>{job.company}</p>
-                <p>{job.from}</p>
-                <p>{job.to}</p>
+                <div className="f-t">
+                  <p>From {job.from}</p>
+                  <p>To {job.to}</p>
+                </div>
               </div>
             );
           })}
